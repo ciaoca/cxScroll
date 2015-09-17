1eadbb425e009e94d149a1dcec5db611a074f53f
ciaoca <ciaoca@gmail.com>
2015-09-17 11:09:18 +0800

    update 1.2.2

--- README.md
+++ README.md
@@ -4,107 +4,38 @@
 
 **版本：**
 
-* jQuery v1.4.4+
-* jQuery cxScroll v1.2.1
+* jQuery v1.7+
+* jQuery cxScroll v1.2.2
 
 **注意事项：**
 
 1. 内部会自动创建 prev 及 next 切换按钮，也可以在外部直接创建，若外部已创建或设置 prevBtn:false 及 nextBtn:false ，内部将跳过此步骤；
-2. 若（滚动的列表宽度 ≤ 父元素的宽度），则不会自动创建 prev 及 next 切换按钮，也不会自动滚动；
-3. 鼠标移动到滚动元素上时，会暂停滚动，移出后会恢复自动滚动（仅在设置 auto:true 时有效）。
+2. 若（滚动的列表宽度 ≤ 父元素的宽度），则不会自动创建 prev 及 next 切换按钮，也不会自动滚动。
 
 文档：http://code.ciaoca.com/jquery/cxscroll/
 
 示例：http://code.ciaoca.com/jquery/cxscroll/demo/
 
-##【options 参数说明】
-
-<table>
-    <tr>
-        <th width="80">名称</th>
-        <th width="80">默认值</th>
-        <th>说明</th>
-    </tr>
-    <tr>
-        <td>direction</td>
-        <td>"right"</td>
-        <td>滚动方向。可设置为："left"|"right"|"top"|"bottom"</td>
-    </tr>
-    <tr>
-        <td>easing</td>
-        <td>"swing"</td>
-        <td>缓动方式</td>
-    </tr>
-    <tr>
-        <td>step</td>
-        <td>1</td>
-        <td>滚动步长</td>
-    </tr>
-    <tr>
-        <td>accel</td>
-        <td>160</td>
-        <td>手动滚动速度(ms)</td>
-    </tr>
-    <tr>
-        <td>speed</td>
-        <td>800</td>
-        <td>自动滚动速度(ms)</td>
-    </tr>
-    <tr>
-        <td>time</td>
-        <td>4000</td>
-        <td>自动滚动间隔时间(ms)</td>
-    </tr>
-    <tr>
-        <td>auto</td>
-        <td>true</td>
-        <td>是否自动滚动</td>
-    </tr>
-    <tr>
-        <td>prevBtn</td>
-        <td>true</td>
-        <td>是否使用 prev 按钮</td>
-    </tr>
-    <tr>
-        <td>nextBtn</td>
-        <td>true</td>
-        <td>是否使用 next 按钮</td>
-    </tr>
-    <tr>
-        <td>safeLock</td>
-        <td>true</td>
-        <td>滚动时是否锁定控制按钮</td>
-    </tr>
-</table>
-
-
-##【使用方法】
+##使用方法
 
 ###CSS 样式结构
+除必要属性设置外，其他样式均可自行设置。
 ```css
-/* 横向滚动参考样式 */
-.cxscroll{position:relative;width:660px;height:120px;padding:10px;background-color:#eee;}
-.cxscroll .box{overflow:hidden;position:relative;width:580px;height:120px;margin:0 auto;}
+/* 横向滚动基本样式 */
+.cxscroll{}
+.cxscroll .box{overflow:hidden;width:600px;}
 .cxscroll .list{overflow:hidden;width:9999px;}
-.cxscroll .list li{float:left;width:200px;}
-.cxscroll .prev,
-.cxscroll .next{position:absolute;top:50%;margin-top:-25px;width:40px;height:50px;background-image:url(img/control.png);background-repeat:no-repeat;font:0/0 Arial;cursor:pointer;}
-.cxscroll .prev{left:5px;background-position:0 0;}
-.cxscroll .next{right:5px;background-position:-40px 0;}
-.cxscroll .prev:hover{background-position:0 -50px;}
-.cxscroll .next:hover{background-position:-40px -50px;}
+.cxscroll .list li{float:left;width:200px;height:100px;}
+.cxscroll .prev{}
+.cxscroll .next{}
 
-/* 纵向滚动参考样式 */
-.cxscroll{position:relative;width:180px;height:340px;padding:10px;background:#eee;}
-.cxscroll .box{overflow:hidden;position:relative;top:40px;height:260px;}
-.cxscroll .list{position:absolute;top:0;left:0;}
-.cxscroll .list li{height:140px;text-align:center;}
-.cxscroll .prev,
-.cxscroll .next{position:absolute;left:50%;width:120px;height:30px;margin-left:-25px;width:50px;height:40px;background-image:url(img/control.png);background-repeat:no-repeat;font:0/0 Arial;cursor:pointer;}
-.cxscroll .prev{top:5px;background-position:-80px 0;}
-.cxscroll .next{bottom:5px;background-position:-130px 0;}
-.cxscroll .prev:hover{background-position:-80px -40px;}
-.cxscroll .next:hover{background-position:-130px -40px;}
+/* 纵向滚动基本样式 */
+.cxscroll{}
+.cxscroll .box{overflow:hidden;height:300px;}
+.cxscroll .list{}
+.cxscroll .list li{height:100px;}
+.cxscroll .prev{}
+.cxscroll .next{}
 ```
 
 ###DOM 结构
@@ -118,7 +49,7 @@
             <li></li>
         </ul>
     </div>
-    <!-- 控制按钮内部会自动创建，可省略 -->
+    <!-- 控制按钮会自动创建，可省略 -->
     <a class="prev"></a>
     <a class="next"></a>
 </div>
@@ -126,19 +57,108 @@
 
 ###调用 cxScroll
 ```javascript
-// 直接调用
 $("#element_id").cxScroll();
+```
 
-// 自定义参数调用
-$("#element_id").cxScroll({
-    direction:"right",
-    step:1,
-    accel:160,
-    speed:800,
-    time:4000,
-    auto:true,
-    prevBtn:true,
-    nextBtn:true,
-    safeLock:true
+##options 参数说明
+<table>
+  <tr>
+    <th width="80">名称</th>
+    <th width="80">默认值</th>
+    <th>说明</th>
+  </tr>
+  <tr>
+    <td>direction</td>
+    <td>'right'</td>
+    <td>滚动方向。可设置为："left", "right", "top", "bottom"</td>
+  </tr>
+  <tr>
+    <td>easing</td>
+    <td>'swing'</td>
+    <td>缓动方式</td>
+  </tr>
+  <tr>
+    <td>step</td>
+    <td>1</td>
+    <td>滚动步长</td>
+  </tr>
+  <tr>
+    <td>accel</td>
+    <td>200</td>
+    <td>手动滚动速度 (ms)，点击控制按钮滚动的速度。</td>
+  </tr>
+  <tr>
+    <td>speed</td>
+    <td>800</td>
+    <td>自动滚动速度 (ms)</td>
+  </tr>
+  <tr>
+    <td>time</td>
+    <td>4000</td>
+    <td>自动滚动间隔时间 (ms)</td>
+  </tr>
+  <tr>
+    <td>auto</td>
+    <td>true</td>
+    <td>是否自动滚动</td>
+  </tr>
+  <tr>
+    <td>hoverLock</td>
+    <td>true</td>
+    <td>鼠标移入移出锁定。鼠标进入区域内时停止自动轮播，离开后恢复自动轮播（仅在 auto 为 true 时有效）</td>
+  </tr>
+  <tr>
+    <td>prevBtn</td>
+    <td>true</td>
+    <td>是否使用 prev 按钮</td>
+  </tr>
+  <tr>
+    <td>nextBtn</td>
+    <td>true</td>
+    <td>是否使用 next 按钮</td>
+  </tr>
+  <tr>
+    <td>hoverLock</td>
+    <td>true</td>
+    <td>鼠标移入移出锁定</td>
+  </tr>
+</table>
+
+##API接口
+```javascript
+var Api;
+$('#element_id').cxScroll(function(api){
+  Api = api;
+});
+// 或者作为第二个参数传入
+$('#element_id').cxScroll({
+  minus: true,
+  plus: true
+}, function(api){
+  Api = api;
 });
 ```
+<table>
+  <thead>
+    <tr>
+      <th width="200">名称</th>
+      <th>说明</th>
+    </tr>
+  </thead>
+  <tr>
+    <td>play()</td>
+    <td>开始自动播放</td>
+  </tr>
+  <tr>
+    <td>stop()</td>
+    <td>停止自动播放</td>
+  </tr>
+  <tr>
+    <td>prev(speed)</td>
+    <td>向前滚动。speed 为滚动速度（ms）</td>
+  </tr>
+  <tr>
+    <td>next(speed)</td>
+    <td>向后滚动。speed 为滚动速度（ms）</td>
+  </tr>
+</table>
--- js/jquery.cxscroll.js
+++ js/jquery.cxscroll.js
@@ -1,217 +1,332 @@
 /*!
- * jQuery cxScroll 1.2.1
+ * jQuery cxScroll 1.2.2
  * http://code.ciaoca.com/
  * https://github.com/ciaoca/cxScroll
  * E-mail: ciaoca@gmail.com
  * Released under the MIT license
- * Date: 2014-03-10
+ * Date: 2015-09-17
  */
-(function($){
-	$.fn.cxScroll=function(settings){
-		if(!this.length){return};
-		settings=$.extend({},$.cxScroll.defaults,settings);
+(function(factory){
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  } else {
+    factory(jQuery);
+  };
+}(function($){
+  $.cxScroll = function(){
+    var obj;
+    var settings;
+    var callback;
+    var scroller = {
+      dom: {},
+      api: {},
+      lockState: false
+    };
 
-		var obj=this;
-		var scroller={
-			lock:false,
-			dom:{}
-		};
+    // 检测是否为 DOM 元素
+    var isElement = function(o){
+      if(o && (typeof HTMLElement === 'function' || typeof HTMLElement === 'object') && o instanceof HTMLElement) {
+        return true;
+      } else {
+        return (o && o.nodeType && o.nodeType === 1) ? true : false;
+      };
+    };
 
-		scroller.init=function(){
-			scroller.dom.box=obj.find(".box");
-			scroller.dom.list=scroller.dom.box.find(".list");
-			scroller.dom.items=scroller.dom.list.find("li");
-			scroller.itemSum=scroller.dom.items.length;
+    // 检测是否为 jQuery 对象
+    var isJquery = function(o){
+      return (o && o.length && (typeof jQuery === 'function' || typeof jQuery === 'object') && o instanceof jQuery) ? true : false;
+    };
 
-			// 没有元素或只有1个元素时，不进行滚动
-			if(scroller.itemSum<=1){return};
+    // 分配参数
+    for (var i = 0, l = arguments.length; i < l; i++) {
+      if (isJquery(arguments[i])) {
+        obj = arguments[i];
+      } else if (isElement(arguments[i])) {
+        obj = $(arguments[i]);
+      } else if (typeof arguments[i] === 'function') {
+        callback = arguments[i];
+      } else if (typeof arguments[i] === 'object') {
+        settings = arguments[i];
+      };
+    };
 
-			scroller.dom.prevBtn=obj.find(".prev");
-			scroller.dom.nextBtn=obj.find(".next");
-			scroller.itemWidth=scroller.dom.items.outerWidth();
-			scroller.itemHeight=scroller.dom.items.outerHeight();
+    if (!obj.length) {return};
 
-			if(settings.direction=="left"||settings.direction=="right"){
-				// 容器宽度不足时，不进行滚动
-				if(scroller.itemWidth*scroller.itemSum<=scroller.dom.box.outerWidth()){return};
+    scroller.init = function(){
+      var self = this;
 
-				scroller.prevVal="left";
-				scroller.nextVal="right";
-				scroller.moveVal=scroller.itemWidth;
-			}else{
-				// 容器高度不足时，不进行滚动
-				if(scroller.itemHeight*scroller.itemSum<=scroller.dom.box.outerHeight()){return};
+      self.dom.el = obj;
+      self.settings = $.extend({}, $.cxScroll.defaults, settings);
 
-				scroller.prevVal="top";
-				scroller.nextVal="bottom";
-				scroller.moveVal=scroller.itemHeight;
-			};
+      self.build();
 
-			// 元素：后补
-			scroller.dom.list.append(scroller.dom.list.html());
+      self.api = {
+        play: function(){
+          self.settings.auto = true;
+          self.play();
+        },
+        stop: function(){
+          self.settings.auto = false;
+          self.stop();
+        },
+        prev: function(speed){
+          speed = parseInt(speed, 10);
+          if (typeof speed !== 'number' || speed < 0) {
+            speed = self.settings.speed;
+          };
+          self.goto(self.prevVal, speed);
+        },
+        next: function(speed){
+          speed = parseInt(speed, 10);
+          if (typeof speed !== 'number' || speed < 0) {
+            speed = self.settings.speed;
+          };
+          self.goto(self.nextVal, speed);
+        }
+      };
 
-			// 添加元素：手动操作按钮
-			if(settings.prevBtn&&!scroller.dom.prevBtn.length){
-				scroller.dom.prevBtn=$("<a></a>",{"class":"prev"}).prependTo(obj);
-			};
-			if(settings.nextBtn&&!scroller.dom.nextBtn.length){
-				scroller.dom.nextBtn=$("<a></a>",{"class":"next"}).prependTo(obj);
-			};
+      if (typeof callback === 'function') {
+        callback(self.api);
+      };
+    };
 
-			// 事件：鼠标移入停止，移出开始
-			if(settings.auto){
-				obj.hover(function(){
-					settings.auto=false;
-					scroller.lock=false;
-					scroller.off();
-				},function(){
-					settings.auto=true;
-					scroller.lock=false;
-					scroller.on();
-				});
-			};
+    scroller.build = function(){
+      var self = this;
 
-			scroller.bindEvents();
-			scroller.on();
-		};
-		
-		scroller.bindEvents=function(){
-			if(settings.nextBtn&&scroller.dom.prevBtn.length){
-				scroller.dom.nextBtn.bind("click",function(){
-					if(!scroller.lock){
-						scroller.goto(scroller.nextVal,settings.accel);
-					};
-				});
-			};
-			if(settings.prevBtn&&scroller.dom.prevBtn.length){
-				scroller.dom.prevBtn.bind("click",function(){
-					if(!scroller.lock){
-						scroller.goto(scroller.prevVal,settings.accel);
-					};
-				});
-			};
-		};
+      self.dom.box = self.dom.el.find('.box');
+      self.dom.list = self.dom.box.find('.list');
+      self.dom.items = self.dom.list.find('li');
+      self.itemSum = self.dom.items.length;
 
-		// 方法：开始
-		scroller.on=function(){
-			if(!settings.auto){return};
-			if(typeof(scroller.run)!=="undefined"){
-				clearTimeout(scroller.run);
-			};
+      // 没有元素或只有1个元素时，不进行滚动
+      if (self.itemSum <= 1){return};
 
-			scroller.run=setTimeout(function(){
-				scroller.goto(settings.direction);
-			},settings.time);
-		};
+      self.dom.prevBtn = self.dom.el.find('.prev');
+      self.dom.nextBtn = self.dom.el.find('.next');
+      self.itemWidth = self.dom.items.outerWidth();
+      self.itemHeight = self.dom.items.outerHeight();
 
-		// 方法：停止
-		scroller.off=function(){
-			scroller.dom.box.stop(true);
-			if(typeof(scroller.run)!=="undefined"){
-				clearTimeout(scroller.run);
-			};
-		};
+      if (self.settings.direction === 'left' || self.settings.direction === 'right') {
+        // 容器宽度不足时，不进行滚动
+        if (self.itemWidth * self.itemSum <= self.dom.box.outerWidth()) {return};
 
-		// 方法：滚动
-		scroller.goto=function(d,t){
-			scroller.off();
-			if(settings.controlLock){
-				scroller.lock=true;
-			};
+        self.prevVal = 'left';
+        self.nextVal = 'right';
+        self.moveVal = self.itemWidth;
 
-			var _max;	// _max	滚动的最大限度
-			var _dis;	// _dis	滚动的距离
-			var _speed=t||settings.speed;
+      } else {
+        // 容器高度不足时，不进行滚动
+        if (self.itemHeight * self.itemSum <= self.dom.box.outerHeight()){return};
 
-			switch(d){
-				case "left":
-				case "top":
-					_max=0;
-					if(d=="left"){
-						if(parseInt(scroller.dom.box.scrollLeft(),10)==0){
-							scroller.dom.box.scrollLeft(scroller.itemSum*scroller.moveVal);
-						};
-						_dis=scroller.dom.box.scrollLeft()-(scroller.moveVal*settings.step);
-						if(_dis%scroller.itemWidth>0){
-							_dis-=(_dis%scroller.itemWidth)-scroller.itemWidth;
-						};
-						if(_dis<_max){_dis=_max};
-						scroller.dom.box.animate({"scrollLeft":_dis},_speed,settings.easing,function(){
-							if(parseInt(scroller.dom.box.scrollLeft(),10)<=_max){
-								scroller.dom.box.scrollLeft(0);
-							};
-						});
-					}else{
-						if(parseInt(scroller.dom.box.scrollTop(),10)==0){
-							scroller.dom.box.scrollTop(scroller.itemSum*scroller.moveVal);
-						};
-						_dis=scroller.dom.box.scrollTop()-(scroller.moveVal*settings.step);
-						if(_dis%scroller.itemHeight>0){
-							_dis-=(_dis%scroller.itemHeight)-scroller.itemHeight;
-						};
-						if(_dis<_max){_dis=_max};
-						scroller.dom.box.animate({"scrollTop":_dis},_speed,settings.easing,function(){
-							if(parseInt(scroller.dom.box.scrollTop(),10)<=_max){
-								scroller.dom.box.scrollTop(0);
-							};
-						});
-					};
-					break;
-	
-				case "right":
-				case "bottom":
-					_max=scroller.itemSum*scroller.moveVal;
-					if(d=="right"){
-						_dis=scroller.dom.box.scrollLeft()+(scroller.moveVal*settings.step);
-						if(_dis%scroller.itemWidth>0){
-							_dis-=(_dis%scroller.itemWidth);
-						};
-						if(_dis>_max){_dis=_max};
-						scroller.dom.box.animate({"scrollLeft":_dis},_speed,settings.easing,function(){
-							if(parseInt(scroller.dom.box.scrollLeft(),10)>=_max){
-								scroller.dom.box.scrollLeft(0);
-							};
-						});
-					}else{
-						_dis=scroller.dom.box.scrollTop()+(scroller.moveVal*settings.step);
-						if(_dis%scroller.itemHeight>0){
-							_dis-=(_dis%scroller.itemHeight);
-						};
-						if(_dis>_max){_dis=_max};
-						scroller.dom.box.animate({"scrollTop":_dis},_speed,settings.easing,function(){
-							if(parseInt(scroller.dom.box.scrollTop(),10)>=_max){
-								scroller.dom.box.scrollTop(0);
-							};
-						});
-					};
-					break;
-				
-				// not default
-			};
-			
-			scroller.dom.box.queue(function(){
-				if(settings.controlLock){
-					scroller.lock=false;
-				};
-				scroller.on();
-				$(this).dequeue();
-			});
-		};
+        self.prevVal = 'top';
+        self.nextVal = 'bottom';
+        self.moveVal = self.itemHeight;
+      };
 
-		scroller.init();
-	};
+      // 元素：后补
+      self.dom.list.append(self.dom.list.html());
 
-	// 默认值
-	$.cxScroll={defaults:{
-		direction:"right",	// 滚动方向
-		easing:"swing",		// 缓动方式
-		step:1,				// 滚动步长
-		accel:160,			// 手动滚动速度
-		speed:800,			// 自动滚动速度
-		time:4000,			// 自动滚动间隔时间
-		auto:true,			// 是否自动滚动
-		prevBtn:true,		// 是否使用 prev 按钮
-		nextBtn:true,		// 是否使用 next 按钮
-		safeLock:true		// 滚动时是否锁定控制按钮
-	}};
-})(jQuery);
+      // 添加元素：手动操作按钮
+      if (self.settings.prevBtn && !self.dom.prevBtn.length) {
+        self.dom.prevBtn = $('<a></a>', {'class':'prev'}).prependTo(self.dom.el);
+      };
+      if (self.settings.nextBtn && !self.dom.nextBtn.length) {
+        self.dom.nextBtn = $('<a></a>', {'class':'next'}).prependTo(self.dom.el);
+      };
+
+      // 事件：手动操作
+      if (self.settings.nextBtn && self.dom.prevBtn.length) {
+        self.dom.nextBtn.bind('click', function(){
+          if (!self.lockState) {
+            self.goto(self.nextVal, self.settings.accel);
+          };
+        });
+      };
+      if (self.settings.prevBtn && self.dom.prevBtn.length) {
+        self.dom.prevBtn.bind('click', function(){
+          if (!self.lockState) {
+            self.goto(self.prevVal, self.settings.accel);
+          };
+        });
+      };
+
+      // 事件：鼠标移入停止，移出开始
+      if (self.settings.hoverLock) {
+        self.dom.box.on('mouseenter', function(){
+          self.stop();
+        });
+        self.dom.box.on('mouseleave', function(){
+          self.play();
+        });
+      };
+
+      self.play();
+    };
+
+    // 方法：开始
+    scroller.play = function(){
+      var self = this;
+
+      if (!self.settings.auto) {return};
+      self.stop();
+
+      self.run = setTimeout(function(){
+        self.goto();
+      }, self.settings.time);
+    };
+
+    // 方法：停止
+    scroller.stop = function(){
+      // 立即停止（效果不是很好）
+      // this.dom.box.stop(true);
+      if (typeof(this.run) !== 'undefined') {
+        clearTimeout(this.run);
+      };
+    };
+
+    // 方法：滚动
+    scroller.goto = function(d, t){
+      var self = this;
+      var _max;  // 滚动的最大限度
+      var _dis;  // 滚动的距离
+      var _speed = t || self.settings.speed;
+
+      if (typeof d !== 'string') {
+        d = self.settings.direction;
+      };
+
+      self.stop();
+      self.lockState = true;
+
+      switch(d) {
+        case 'left':
+        case 'top':
+          _max = 0;
+
+          if (d === 'left') {
+            if (parseInt(self.dom.box.scrollLeft(), 10) === 0) {
+              self.dom.box.scrollLeft(self.itemSum * self.moveVal);
+            };
+
+            _dis = self.dom.box.scrollLeft() - (self.moveVal * self.settings.step);
+
+            if (_dis % self.itemWidth > 0) {
+              _dis -= (_dis % self.itemWidth) - self.itemWidth;
+            };
+
+            if (_dis < _max) {
+              _dis = _max;
+            };
+
+            self.dom.box.animate({
+              'scrollLeft': _dis
+            }, _speed, self.settings.easing, function(){
+              if (parseInt(self.dom.box.scrollLeft(), 10) <= _max) {
+                self.dom.box.scrollLeft(0);
+              };
+            });
+
+          } else {
+            if (parseInt(self.dom.box.scrollTop(), 10) === 0) {
+              self.dom.box.scrollTop(self.itemSum * self.moveVal);
+            };
+
+            _dis = self.dom.box.scrollTop() - (self.moveVal * self.settings.step);
+
+            if (_dis % self.itemHeight > 0) {
+              _dis-=(_dis%self.itemHeight)-self.itemHeight;
+            };
+
+            if (_dis < _max){
+              _dis = _max;
+            };
+
+            self.dom.box.animate({
+              'scrollTop': _dis
+            }, _speed, self.settings.easing, function(){
+              if (parseInt(self.dom.box.scrollTop(), 10) <= _max) {
+                self.dom.box.scrollTop(0);
+              };
+            });
+          };
+          break;
+  
+        case 'right':
+        case 'bottom':
+          _max = self.itemSum * self.moveVal;
+
+          if (d === 'right'){
+            _dis = self.dom.box.scrollLeft() + (self.moveVal * self.settings.step);
+
+            if (_dis % self.itemWidth > 0) {
+              _dis -= (_dis % self.itemWidth);
+            };
+
+            if (_dis > _max){
+              _dis = _max;
+            };
+
+            self.dom.box.animate({
+              'scrollLeft': _dis
+            }, _speed, self.settings.easing, function(){
+              if (parseInt(self.dom.box.scrollLeft(), 10) >= _max) {
+                self.dom.box.scrollLeft(0);
+              };
+            });
+
+          } else {
+            _dis = self.dom.box.scrollTop() + (self.moveVal * self.settings.step);
+
+            if (_dis % self.itemHeight > 0){
+              _dis -= (_dis % self.itemHeight);
+            };
+
+            if (_dis > _max){
+              _dis = _max;
+            };
+
+            self.dom.box.animate({
+              'scrollTop': _dis
+            }, _speed, self.settings.easing, function(){
+              if (parseInt(self.dom.box.scrollTop(), 10) >= _max) {
+                self.dom.box.scrollTop(0);
+              };
+            });
+          };
+          break;
+        
+        default:
+          return;
+      };
+      
+      self.dom.box.queue(function(){
+        self.lockState = false;
+        self.play();
+        $(this).dequeue();
+      });
+    };
+
+    scroller.init();
+  };
+
+  // 默认值
+  $.cxScroll.defaults = {
+    direction: 'right',   // 滚动方向
+    easing: 'swing',      // 缓动方式
+    step: 1,              // 滚动步长
+    accel: 200,           // 手动滚动速度
+    speed: 800,           // 自动滚动速度
+    time: 4000,           // 自动滚动间隔时间
+    auto: true,           // 是否自动滚动
+    hoverLock: true,      // 鼠标移入移出锁定
+    prevBtn: true,        // 是否使用 prev 按钮
+    nextBtn: true         // 是否使用 next 按钮
+  };
+
+  $.fn.cxScroll = function(settings, callback){
+    this.each(function(i){
+      $.cxScroll(this, settings, callback);
+    });
+    return this;
+  };
+}));
